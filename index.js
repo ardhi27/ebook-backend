350d1bcb8b98be25d55227d3baeed36305cd47f5
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-04-27 13:14:10 +0800

    feat: init dotenv

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mysql from "mysql2";
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/user', (req, res) => {
-    res.send('Hello World');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+//Displaying userlist
+app.get('/userlist', (req, res) => {
+    const select = "SELECT * FROM `user`";
+    connection.query(selectSql, (err, res) => {
+        if (err) {
+            console.error("Error Taking Data : ", err);
+            res.serverStatus(500).send("Error Taking Data")
+        } else {
+            res.json(results);
+        }
+    })
 })
 
 app.listen(port, () => {
