aed9ebaf1fec2fcb40c45a2ddf2ee67f40ce6811
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-04-27 13:25:23 +0800

    feat: add routing for GET and POST data

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mysql from "mysql2";
+import dotenv from 'dotenv';
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -17,12 +20,26 @@ const connection = mysql.createConnection({
 //Displaying userlist
 app.get('/userlist', (req, res) => {
     const select = "SELECT * FROM `user`";
-    connection.query(selectSql, (err, res) => {
+    connection.query(select, (err, results) => {
         if (err) {
             console.error("Error Taking Data : ", err);
-            res.serverStatus(500).send("Error Taking Data")
+            res.status(500).send("Error Taking Data")
         } else {
             res.json(results);
+        }
+    })
+})
+
+//Add user
+app.post('/submit', (req, res) => {
+    const { username, password } = req.body;
+    const insert = "INSERT INTO `user` (username, password) VALUES(?, ?)";
+    connection.query(insert, [username, password], (err, results) => {
+        if (err) {
+            console.error("Error : ", err);
+            res.status(500).send("Error Input Data");
+        } else {
+            res.status(201).send("Succesfuly Added Data");
         }
     })
 })
