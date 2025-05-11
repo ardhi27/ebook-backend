a98d5d0c1ce4168612fcb708956e6c8907c2fa09
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-05-11 16:23:51 +0800

    feat: implement controller and services for user

---
+++ src/api/user/user.services.ts
@@ -0,0 +1,86 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { HttpException } from "../../../shared/http.exception";
+import jwt from "jsonwebtoken";
+
+export class UserService {
+  private db: PrismaClient;
+
+  //Init prismaclient
+  constructor() {
+    this.db = new PrismaClient();
+  }
+
+  //Login
+  async login(loginData: any) {
+    //Finding user if match
+    const user = await this.db.user.findUnique({
+      where: {
+        username: loginData.username,
+      },
+    });
+
+    //if userName doesnt match then throw error 404
+    if (!user) {
+      throw new HttpException(404, "User not found");
+    }
+    //Check isPassword is correct and returning false/true
+    const isPasswordCorrect = await this.comparePassword(
+      loginData.password,
+      user.password
+    );
+
+    //if uncorrect then server send response user not found
+    if (!isPasswordCorrect) {
+      throw new HttpException(404, "User not found");
+    }
+
+    //Returning jwt token for each user
+    return this.createJwtToken(user.id);
+  }
+
+  //Register
+  async register(registerData: any) {
+    //Before user register, the function will check first if the user is exist
+    const user = await this.db.user.findFirst({
+      where: {
+        username: { equals: registerData.username },
+      },
+    });
+
+    //If user is exist, then server will send statuscode 404
+    if (user) {
+      throw new HttpException(404, "User already exist");
+    }
+
+    //When we enter the password, the program will hash the password into hard string character
+    const hashedPassword = await this.hashedPassword(registerData.password);
+
+    const newUser = await this.db.user.create({
+      data: {
+        username: registerData.username,
+        password: hashedPassword,
+      },
+    });
+
+    return newUser.username;
+  }
+
+  private comparePassword(plainPassword: string, hashedPassword: string) {
+    return bcrypt.compare(plainPassword, hashedPassword);
+  }
+
+  private createJwtToken(userId: number) {
+    const jwtToken = process.env.JWT_KEY;
+    if (!jwtToken) {
+      throw new HttpException(500, "JWT_KEY env not found");
+    }
+    return jwt.sign({ userId }, jwtToken, {
+      expiresIn: "14d",
+    });
+  }
+
+  private hashedPassword(plainPassword: string) {
+    return bcrypt.hash(plainPassword, 10);
+  }
+}
