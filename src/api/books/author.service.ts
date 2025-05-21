971b86e6564fbf5bb59901f58663c101a9e695ef
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-05-21 21:14:31 +0800

    feat: implement function to get and delete author

--- src/api/books/author.service.ts
+++ src/api/books/author.service.ts
@@ -9,6 +9,10 @@ export class AuthorService {
     this.db = prisma;
   }
 
+  async viewAuthor() {
+    return await this.db.booksAuthor.findMany();
+  }
+
   async createAuthor(authorData: AuthorDto) {
     if (!authorData.authorName) {
       throw new HttpException(404, "Author must required");
@@ -19,6 +23,7 @@ export class AuthorService {
         author: authorData.authorName,
       },
     });
+
     if (isAuthorExisted) {
       throw new HttpException(409, "Author already existed");
     }
@@ -28,10 +33,51 @@ export class AuthorService {
         author: authorData.authorName,
       },
     });
+  }
+
+  async deleteAuthor(authorId: number, authorData: AuthorDto) {
+    const findAuthor = await this.db.booksAuthor.findFirst({
+      where: {
+        authorId: authorId,
+      },
+    });
+
+    if (!findAuthor) {
+      throw new HttpException(404, "Author  not found");
+    }
+
+    await this.db.booksAuthor.delete({
+      where: {
+        authorId: authorId,
+      },
+    });
+  }
+
+  async updateAuthor(authorId: number, authorData: AuthorDto) {
+    const findAuthor = await this.db.booksAuthor.findFirst({
+      where: {
+        authorId: authorId,
+      },
+    });
+
+    if (!findAuthor) {
+      throw new HttpException(404, "Author not found");
+    }
+
+    await this.db.booksAuthor.update({
+      where: {
+        authorId: authorId,
+      },
+      data: {
+        author: authorData.authorName,
+      },
+    });
 
     return {
-      status: "success",
-      data: newAuthor,
+      message: "Success",
+      data: {
+        author: authorData.authorName,
+      },
     };
   }
 }
