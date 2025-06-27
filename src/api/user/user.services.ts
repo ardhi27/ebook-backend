faaa156a7031aa4960026d4a26949bb7ef9e81a9
kmshyn76 <147911990+Ardhi27@users.noreply.github.com>
2025-06-27 15:18:46 +0800

    feat: add user role parameter to store the role's data in jwt

--- src/api/user/user.services.ts
+++ src/api/user/user.services.ts
@@ -8,62 +8,47 @@ import UserRegisterProps from "./user.register.dto";
 export class UserService {
   private db: PrismaClient;
 
-  //Init prismaclient
   constructor() {
     this.db = prisma;
   }
 
-  //Login
   async login(loginData: UserLoginProps) {
-    //Finding user if match
     const user = await this.db.user.findFirst({
       where: {
-        // username: loginData.username,
         username: loginData.username,
       },
     });
 
-    //if userName doesnt match then throw error 404
     if (!user) {
       throw new HttpException(404, "User not found");
     }
-    //Check isPassword is correct and returning false/true
     const isPasswordCorrect = await this.comparePassword(
       loginData.password,
       user.password
     );
 
-    //if uncorrect then server send response user not found
     if (!isPasswordCorrect) {
       throw new HttpException(404, "User not found");
     }
 
-    //Returning jwt token for each user
-    return this.createJwtToken(user.userId);
+    return this.createJwtToken(user.userId, user.userRole);
   }
 
-  //Register
   async register(registerData: UserRegisterProps) {
     console.log(registerData.username);
-    // if (!registerData.username) {
-    //   throw new HttpException(409, "Username must required");
-    // }
-    //Before user register, the function will check first if the user is exist
+
     const user = await this.db.user.findFirst({
       where: {
         username: { equals: registerData.username },
       },
     });
 
-    //If user is exist, then server will send statuscode 404
     if (user) {
       throw new HttpException(404, "User already exist");
     }
 
-    //When we enter the password, the program will hash the password into hard string character
     const hashedPassword = await this.hashedPassword(registerData.password);
 
-    //Create a new user
     const newUser = await this.db.user.create({
       data: {
         username: registerData.username,
@@ -101,12 +86,12 @@ export class UserService {
     return bcrypt.compare(plainPassword, hashedPassword);
   }
 
-  private createJwtToken(userId: number) {
+  private createJwtToken(userId: number, userRole: string) {
     const jwtToken = process.env.JWT_KEY;
     if (!jwtToken) {
       throw new HttpException(500, "JWT_KEY env not found");
     }
-    return jwt.sign({ userId }, jwtToken, {
+    return jwt.sign({ userId, userRole }, jwtToken, {
       expiresIn: "14d",
     });
   }
